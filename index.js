bf774de1e7c601992f3c7348a96c7891c07ab39f
Afraaz <ahmedalif102@gmail.com>
2023-01-15 21:01:37 +0600

    jwt installed

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 const app = express()
+const jwt = require('jsonwebtoken');
+const dotenv = require("dotenv");
+
+require("dotenv").config();
 
 //middlewares
 
