6d8103101a9763203641dd7673516a3241e025ff
Afraaz <ahmedalif102@gmail.com>
2023-01-15 20:08:03 +0600

    basic setup done

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
 const express = require('express');
 const cors = require('cors');
+const port = process.env.PORT || 5000;
+const app = express()
+
+//middlewares
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', async (req, res) => {
+  res.send('Nerd Academy Server Is Running')
+})
+
+app.listen(port , () => {
+  console.log(`Nerd Academy Server Is Running On ${port}`);
+})
