edfca7b406bf831c99916ce7cb28631b784c7178
ahm-rubayed <rubayedahmed04@gmail.com>
2023-02-01 18:56:20 +0600

    counter api added

--- index.js
+++ index.js
@@ -52,6 +52,12 @@ async function run() {
       res.send(result);
     });
 
+    app.get("/counter", async (req, res) => {
+      const query = {};
+      const result = await counter.find(query).toArray();
+      res.send(result);
+    });
+
     app.get("/courses/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: ObjectId(id) };
